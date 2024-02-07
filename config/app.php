3f1da208341e84604511c9d75f3ab5686ff1cfe1
Saidul Islam <saidulislam197120@gmail.com>
2024-02-07 19:01:02 +0600

    Set some configuration for nwidarth package

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Nwidart\Modules\LaravelModulesServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Module' => Nwidart\Modules\Facades\Module::class,
     ])->toArray(),
 
 ];
